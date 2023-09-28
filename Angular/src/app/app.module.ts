c1eabb0d50f488d563c48fb4b77ff7b7e514fff6
dotnet-code <@dotnet-code>
2023-09-28 12:54:34 +0000

    commit from code202309285434

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { LoanApprovalComponent } from './loan-approval/loan-approval.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsletterComponent,
-    RepositoryComponent
+    RepositoryComponent,
+    LoanApprovalComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/loan-approval/loan-approval.component.ts
@@ -0,0 +1,43 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'path/to/api.service';
+
+@Component({
+  selector: 'app-loan-approval',
+  templateUrl: './loan-approval.component.html',
+  styleUrls: ['./loan-approval.component.css']
+})
+export class LoanApprovalComponent implements OnInit {
+
+  loanApprovalForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loanApprovalForm = this.formBuilder.group({
+      identification: ['', Validators.required],
+      proofOfIncome: ['', Validators.required],
+      creditHistory: ['', Validators.required],
+      employmentDetails: ['', Validators.required]
+    });
+  }
+
+  submitLoanApprovalForm(): void {
+    if (this.loanApprovalForm.invalid) {
+      return;
+    }
+
+    // Call API to approve the loan with specific terms and conditions
+    this.apiService.approveLoan(this.loanApprovalForm.value).subscribe(
+      response => {
+        // Handle success response
+        console.log('Loan approved successfully');
+      },
+      error => {
+        // Handle error response
+        console.error('Failed to approve loan', error);
+      }
+    );
+  }
+
+}
